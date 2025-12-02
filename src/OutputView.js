ffa7545248a34a843186b353805aa11a85b6ab4d
Donghwan Oh <imodh97@gmail.com>
2025-12-02 20:54:45 +0900

    feat(outputView): 출력 뷰 기능 구현

---
+++ src/OutputView.js
@@ -0,0 +1,21 @@
+import { Console } from '@woowacourse/mission-utils';
+
+class OutputView {
+  printResultHeader() {
+    Console.print('\n실행 결과');
+  }
+  printRoundResult(cars) {
+    cars.forEach((car) =>
+      Console.print(`${car.getName()} : ${'-'.repeat(car.getPosition())}`)
+    );
+    Console.print('');
+  }
+
+  printWinners(winners) {
+    Console.print(
+      `최종 우승자 : ${winners.map((winner) => winner.getName()).join(', ')}`
+    );
+  }
+}
+
+export default OutputView;
