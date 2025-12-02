109d3745d52dce945f47c677a441c3ef8074a703
Donghwan Oh <imodh97@gmail.com>
2025-12-02 20:55:25 +0900

    feat(app): 앱 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import GameController from './GameController.js';
+import InputView from './InputView.js';
+import NumberGenerator from './NumberGenerator.js';
+import OutputView from './OutputView.js';
+
 class App {
-  async run() {}
+  async run() {
+    const inputView = new InputView();
+    const outputView = new OutputView();
+    const numberGenerator = new NumberGenerator();
+
+    const gameController = new GameController(
+      inputView,
+      outputView,
+      numberGenerator
+    );
+    await gameController.play();
+  }
 }
 
 export default App;
