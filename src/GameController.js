8cd8319fe1c8524fd068f975cbe2f05cff9d27ba
Donghwan Oh <imodh97@gmail.com>
2025-12-03 16:43:43 +0900

    feat(gameController): 예상 가능한 예외 발생시 재입력 로직 추가

--- src/GameController.js
+++ src/GameController.js
@@ -1,5 +1,8 @@
 import Car from './Car.js';
 import Cars from './Cars.js';
+import { CarError } from './errors/CarError.js';
+import { CarsError } from './errors/CarsError.js';
+import { InputError } from './errors/InputError.js';
 
 class GameController {
   #inputView;
@@ -13,19 +16,34 @@ class GameController {
   }
 
   async play() {
-    const carNames = await this.#inputView.getCarNames();
-    const trialCount = await this.#inputView.getTrialCount();
+    while (true) {
+      try {
+        const carNames = await this.#inputView.getCarNames();
+        const trialCount = await this.#inputView.getTrialCount();
 
-    const cars = new Cars(carNames.map((name) => new Car(name)));
+        const cars = new Cars(carNames.map((name) => new Car(name)));
 
-    this.#outputView.printResultHeader();
-    for (let i = 0; i < trialCount; i++) {
-      cars.moveAll(this.#numberGenerator);
-      this.#outputView.printRoundResult(cars.getCars());
+        this.#outputView.printResultHeader();
+        for (let i = 0; i < trialCount; i++) {
+          cars.moveAll(this.#numberGenerator);
+          this.#outputView.printRoundResult(cars.getCars());
+        }
+
+        const winners = cars.calculateWinners();
+        this.#outputView.printWinners(winners);
+        return;
+      } catch (error) {
+        if (
+          error instanceof InputError ||
+          error instanceof CarsError ||
+          error instanceof CarError
+        ) {
+          this.#outputView.printErrorMessage(error.message);
+        } else {
+          throw error;
+        }
+      }
     }
-
-    const winners = cars.calculateWinners();
-    this.#outputView.printWinners(winners);
   }
 }
 
