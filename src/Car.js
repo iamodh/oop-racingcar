5b744a04c9153987ecd1dd0183bfe801d66cc68a
Donghwan Oh <imodh97@gmail.com>
2025-12-03 14:33:28 +0900

    refactor(car): 에러 메시지 상수화

--- src/Car.js
+++ src/Car.js
@@ -2,6 +2,9 @@ class Car {
   #name;
   #position;
   static NAME_LENGTH = { MIN: 1, MAX: 5 };
+  static ERROR = {
+    NAME_LENGTH: `[ERROR] 자동차 이름은 ${Car.NAME_LENGTH.MIN}자 이상 ${Car.NAME_LENGTH.MAX}자 이하로 입력해주세요.`,
+  };
   static MIN_NUMBER_TO_MOVE = 4;
   static MOVEMENT_DISTANCE = 1;
 
@@ -16,7 +19,7 @@ class Car {
       name.length < Car.NAME_LENGTH.MIN ||
       name.length > Car.NAME_LENGTH.MAX
     ) {
-      throw new Error('[ERROR] 유효하지 않은 자동차 이름입니다.');
+      throw new Error(Car.ERROR.NAME_LENGTH);
     }
   }
 
